7c6cfab86e2622aaff63ccffb80f372be61a0a17
SSMZHN <ssmzhn@163.com>
2021-08-08 20:18:59 +0800

    caps

--- caps-kanji.py
+++ caps-kanji.py
@@ -39,7 +39,7 @@ def main(isc=False):
     print(str2)
 
 opt=['-c','--chinese','-h','--help']
-
+import re
 if len(argv)<=1:
     print('Usage: python '+argv[0]+' [options] <string>')
     print('    For example: python '+argv[0]+' hello world')
@@ -49,7 +49,9 @@ if len(argv)<=1:
 elif argv[1][0]=='-' and not(argv[1] in opt):
     print('No such option. Type -h or --help for help.')
 elif argv[1]=='-c' or argv[1]=='--chinese':
-    main(True)
+    if re.match('^哼{3}啊+$',' '.join(argv[2:])):
+        print('臭死力')
+    else:main(True)
 elif argv[1]=='-h' or argv[1]=='--help':
     print('Usage: python '+argv[0]+' [options] <string>')
     print('    For example: python '+argv[0]+' hello world')
@@ -57,4 +59,6 @@ elif argv[1]=='-h' or argv[1]=='--help':
     print('    Another example: python '+argv[0]+' [-c] 你好')
     print('-h|--help: get help')
 else:
-    main(False)
+    if re.match('^哼{3}啊+$',' '.join(argv[2:])):
+        print('臭死力')
+    else:main(False)
